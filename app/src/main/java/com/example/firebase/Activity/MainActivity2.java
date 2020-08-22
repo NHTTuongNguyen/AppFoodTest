a2e809ee9927f79da401a71afbacab400100ac9e
TuongNguyenX <44676265+TuongNguyenX@users.noreply.github.com>
2020-08-22 14:39:01 +0700

    second

---
+++ app/src/main/java/com/example/firebase/Activity/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.firebase.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.firebase.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
