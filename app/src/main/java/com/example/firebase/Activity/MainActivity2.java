cec02a690ca9eab22d8630d55d6251fb15acb1f0
TuongNguyenX <44676265+TuongNguyenX@users.noreply.github.com>
2020-08-22 14:50:58 +0700

    add funtion test()

--- app/src/main/java/com/example/firebase/Activity/MainActivity2.java
+++ app/src/main/java/com/example/firebase/Activity/MainActivity2.java
@@ -12,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        test();
+    }
+
+    private void test() {
     }
 }
